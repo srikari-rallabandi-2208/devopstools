cb073fcba15cde945f3b5b583637291b7badfa1d
Srikari Rallabandi <sreeku.ralla@gmail.com>
2023-07-13 11:11:17 +0530

    Removed the App URLs from the Project Level

--- urls.py
+++ urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from devopstools.views import save_top_results, top_results
 
 urlpatterns = [
-    path('api/save-top-results/', save_top_results, name='save_top_results'),
-    path('top-results/', top_results, name='top_results'),
+
 ]
